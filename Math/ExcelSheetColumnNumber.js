d7674e49ca55fe6ca796759c294764db27e41fe0
John-Don-Bosco <114931534+John-Don-Bosco@users.noreply.github.com>
2022-10-03 19:34:30 +0530

    Create ExcelSheetColumnNumber.js

---
+++ Math/ExcelSheetColumnNumber.js
@@ -0,0 +1,11 @@
+var titleToNumber = function(s) {
+    let result = 0;
+    for (let i = 0; i < s.length; i++)
+    {
+        result *= 26;
+        result += s[i].charCodeAt(0) - 'A'.charCodeAt(0) + 1;
+    }
+    return result;
+}
+
+//Leetcode 171
