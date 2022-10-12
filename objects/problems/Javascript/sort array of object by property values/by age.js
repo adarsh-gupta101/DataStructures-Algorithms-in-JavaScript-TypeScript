3de93edf1337ee974e94b7bfb22908ec3705c498
Sanil Pal <sanil@Sanils-MacBook-Air.local>
2022-10-12 21:46:37 +0530

    sort array of object by property values

---
+++ objects/problems/Javascript/sort array of object by property values/by age.js
@@ -0,0 +1,8 @@
+function compareAge(a, b) {
+
+    return a.age - b.age;
+}
+
+const students = [{name: 'sharukh', age:24},{name: 'akshay', age:24}, {name: 'kunal', age:25}];
+
+console.log(students.sort(compareName));
---
+++ objects/problems/Javascript/sort array of object by property values/by name.js
@@ -0,0 +1,19 @@
+function compareName(a, b) {
+
+    // converting to uppercase to have case-insensitive comparison
+    const name1 = a.name.toUpperCase();
+    const name2 = b.name.toUpperCase();
+
+    let comparison = 0;
+
+    if (name1 > name2) {
+        comparison = 1;
+    } else if (name1 < name2) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+const students = [{name: 'sharukh', age:24},{name: 'akshay', age:24}, {name: 'kunal', age:25}];
+
+console.log(students.sort(compareName));
