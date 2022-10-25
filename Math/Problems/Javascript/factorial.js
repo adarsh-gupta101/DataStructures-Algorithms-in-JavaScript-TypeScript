2b8ead74237bbf38830efc3b7c3ae372a8d23512
Abhinav Kumar <68495520+abhinav2712@users.noreply.github.com>
2022-10-25 12:40:16 +0530

    Update factorial.js

--- Math/Problems/Javascript/factorial.js
+++ Math/Problems/Javascript/factorial.js
@@ -1,23 +1,9 @@
-// program to find the factorial of a number
-
-// take input from the user
-const number = parseInt(prompt('Enter a positive integer: '));
-
-// checking if number is negative
-if (number < 0) {
-   return -1;
-}
-
-// if number is 0
-else if (number === 0) {
-    return 1;
-}
-
-// if number is positive
-else {
-    let fact = 1;
-    for (i = 1; i <= number; i++) {
-        fact *= i;
-    }
-    return fact;
+function factorial(num) {
+  if (num < 0) 
+        return -1;
+  else if (num == 0) 
+      return 1;
+  else {
+      return (num * factorial(num - 1));
+  }
 }
